d3b2adf1d5dd8686159a7c70d3a7d3afffb05e60
dabaixiaozhu <3073203276@qq.com>
2023-01-12 22:10:50 +0800

    【标题】更新为更好管理的get 【修改人】linzy 【修改内容】

--- testjavahttpservice/http-consumer/src/main/java/com/lin/web/HelloController.java
+++ testjavahttpservice/http-consumer/src/main/java/com/lin/web/HelloController.java
@@ -2,15 +2,16 @@ package com.lin.web;
 
 import cn.hutool.http.HttpUtil;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// http-provider:8081
 @RestController
 public class HelloController {
 
-    @GetMapping("/http")
-    public String hello() {
+    @GetMapping("/http/{address}")
+    public String hello(@PathVariable String address) {
         // 使用http访问 provider 的 hello 接口
-        String result = HttpUtil.get("http://http-provider/hello");
-        return "从provider中接收到：" + result;
+        return "从provider中接收到：" + HttpUtil.get(address + "/hello");
     }
 }
